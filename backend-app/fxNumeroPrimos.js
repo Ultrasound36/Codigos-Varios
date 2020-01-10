2d9d76529e0e64bbc1290c50fa0ff770d6b80cfc
Mauricio Águila <prototipo00@gmail.com>
2020-01-10 00:28:23 -0300

    Cambios locales

--- backend-app/fxNumeroPrimos.js
+++ backend-app/fxNumeroPrimos.js
@@ -1,37 +1,37 @@
-var c = 50
-var j = 1
+var c = 50;
+var j = 1;
 
-console.log('La cantidad de numeros semiPrimos son :: ' + findSemiprimes(j, c))
+console.log('La cantidad de numeros semiPrimos son :: ' + findSemiprimes(j, c));
 
 function primo (numero) {
   for (var i = 2; i < numero; i++) {
     if (numero % i === 0) {
-      return false
+      return false;
     }
   }
-  return numero !== 1
+  return numero !== 1;
 }
 
 function findSemiprimes (rangeStart, rangeEnd) {
-  var numerosPrimos = []
-  var numerosSemiPrimos = []
+  var numerosPrimos = [];
+  var numerosSemiPrimos = [];
     
   for (var index = rangeStart; index < rangeEnd; index++) {
     if (primo(index)) {
-      numerosPrimos.push(index)
+      numerosPrimos.push(index);
     }
   }
 
   for (var x = 0; x < numerosPrimos.length; x++) {
-    var posY = numerosPrimos[x]
-    var posX = numerosPrimos[x]
-    var multi = posY * posX
+    var posY = numerosPrimos[x];
+    var posX = numerosPrimos[x];
+    var multi = posY * posX;
 
     if (Number.isNaN(multi)) {
-      numerosSemiPrimos.pop(multi)
+      numerosSemiPrimos.pop(multi);
     } else {
-      numerosSemiPrimos.push(multi)
+      numerosSemiPrimos.push(multi);
     }
   }
-  return numerosSemiPrimos
+  return numerosSemiPrimos;
 }
